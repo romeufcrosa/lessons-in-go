845af6e70ec1c1ba1e6a5c0742ec8013defad55a
Romeu Rosa <romeurosa@Romeus-MacBook-Pro.local>
2024-12-01 22:29:02 +0000

    Add first test for lesson 1

---
+++ lessons/lesson1.go
@@ -0,0 +1,7 @@
+package lessons
+
+import "fmt"
+
+func Lesson1PrintFunc(name string) {
+	fmt.Printf("Hello %s, welcome to Lessons in Go", name)
+}
---
+++ lessons/lesson1_test.go
@@ -0,0 +1,59 @@
+package lessons
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"testing"
+)
+
+const (
+	Reset  = "\033[0m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Purple = "\033[35m"
+	Cyan   = "\033[36m"
+	White  = "\033[37m"
+)
+
+func TestLesson1PrintFunc(t *testing.T) {
+	output := captureOutput(func() { Lesson1PrintFunc("") })
+	compare(t, "Hello Bruno, welcome to Lessons in Go", output)
+}
+
+func compare(t *testing.T, expected, actual interface{}, _ ...any) bool {
+	if expected != actual {
+		t.Errorf(`
+		%sThe path to enlightenment is fraught with errors.
+		%sExpected: %s%q
+		%sBut found: %s%q
+		%sReflect on this and try again.%s`, Green, Blue, Reset, expected, Red, Reset, actual, Cyan, Reset)
+		return false
+	}
+	t.Log("You have successfully passed this test, congratulations.")
+	return true
+}
+
+func captureOutput(f func()) string {
+	r, w, _ := os.Pipe()
+	original := os.Stdout
+	os.Stdout = w
+
+	f()
+
+	err := w.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Stdout = original
+
+	return buf.String()
+}
